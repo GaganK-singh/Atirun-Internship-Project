33af89e5ab5832fca5855bac4556fc6e083c04e1
Money <gks184062@gmail.com>
2023-06-20 04:51:05 +0530

    Getting Docs from FIrebase

--- erpsys/src/Page/Contnt/Borrow.jsx
+++ erpsys/src/Page/Contnt/Borrow.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { db } from "../../FirebaseConfig";
+import { getFirestore, collection, getDocs, addDoc, getDoc } from "firebase/firestore";
 
 export default function Borrow() {
   const [currentState, setCurrentState] = useState({
     bookName: "",
     LibId: "",
-    quantity: ""
+    quantity: "",
   });
 
   const postData = (tag) => {
@@ -14,9 +16,29 @@ export default function Borrow() {
     setCurrentState({ ...currentState, [name]: value });
   };
 
+  // const borrowFormTag = document.getElementById("borrowFormSubmit");
+  // borrowFormTag((event) => {
+  //   event.preventDefault();
+
+  //   addDoc(colRefBorrow, {
+  //     bookName: borrowFormTag.currentState.bookName,
+  //     LibId: Borrow.currentState.LibId,
+  //     quantity: Borrow.currentState.quantity,
+  //   });
+  // });
+
+  async function setBorrow(db) {
+    const colRefBorrow = collection(db, "borrow");
+    const userInstnce = await getDocs(colRefBorrow);
+    const userList = userInstnce.docs.map(doc => doc.data);
+    console.log(userList);
+  }
+
+  useEffect (() => {setBorrow(db);})
+
   return (
     <div>
-      <form>
+      <form id="borrowForm">
         <div className="form-group">
           <label htmlFor="exampleFormControlTextarea1" className="form-label">
             Name Of the book:
@@ -62,7 +84,7 @@ export default function Borrow() {
           />
         </div>
         <br />
-        <button type="submit" className="btn btn-primary">
+        <button type="submit" id="borrowFormSubmit" className="btn btn-primary">
           Submit
         </button>
       </form>

2c5a61eb30609df517091fea574767b307daf909
Money <gks184062@gmail.com>
2023-06-24 04:08:12 +0530

    Another Commit

--- erpsys/src/Page/Contnt/List.jsx
+++ erpsys/src/Page/Contnt/List.jsx
@@ -8,6 +8,7 @@ export default function List() {
   const collectionRef = collection(db, "borrow");
   const getdata = async () => {
     // Here in getDocs() Function we either pass collectionRef or q(which is used for query)
+    // We can use query to sort and search data through a particular name or id (this helps admin the most)
     const querySnapshot = await getDocs(collectionRef);
     querySnapshot.forEach((doc) => {
       console.log(doc.id, " => ", doc.data());
@@ -17,7 +18,12 @@ export default function List() {
   return (
     <div id="list-content-page">
       <h1>List of Borrowed Books</h1>
-      <button type="button" class="btn btn-info" onClick={getdata}>Get List</button>
+      <button type="button" class="btn btn-info" onClick={getdata}>Get List</button><br /> <br />
+      <div>
+        <ul class="list-group" id='bookList'>
+          <li class="list-group-item">Cras justo odio</li>
+        </ul>
+      </div>
     </div>
   )
 }
--- erpsys/src/Page/Contnt/Return.jsx
+++ erpsys/src/Page/Contnt/Return.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Return() {
   return (
-    <div id="return-content-page">Return</div>
+    <div id="return-content-page">
+      <h1>Sorry! This Function is Not Available Yet...</h1>
+    </div>
   )
 }
